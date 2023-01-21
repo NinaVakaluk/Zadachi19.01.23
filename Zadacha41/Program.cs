a2e456240e5d9df859afa2f0655ccf390076d5b9
Nina <you@example.com>
2023-01-21 20:32:49 +0300

    Решение задач 41,43

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел строго больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2   1, -7, 567, 89, 223-> 4
+Для строки можно использовать данный метод string[] numS = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries)(пользователь вводит в консоли 
+числа через пробел и они формируют строку)*/
+
+Console.WriteLine("Введи число:");
+string numS = (Console.ReadLine()!);
+string[] stringArray = numS.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+int[] newArray=new int[stringArray.Length];
+for (int i=0;i<stringArray.Length;i++)
+{
+  newArray[i]=Convert.ToInt32(stringArray[i]);
+} 
+Console.WriteLine($"[{String.Join(",", newArray)}]");
+Console.WriteLine($"Количество чисел больше 0 -> {Quantity(newArray)}");
+
+
+int Quantity(int[] numbers)
+{
+
+   
+    int result = 0;
+    foreach (int N in numbers)
+    {
+        if (N > 0)
+        {
+            result++;
+        }
+    }
+    return result;
+}
+
+
+
---
+++ Zadacha43/Program.cs
@@ -0,0 +1,38 @@
+﻿//Написать программу, которая на вход принимает массив из любого количества элементов (не менее 6)в промежутке от 0 до 100, а на выходе выводит 
+//этот же массив, но отсортированный по возрастанию(от меньшего числа к большему).
+
+int[] Array = GetArray(6);
+Console.WriteLine($"[{String.Join(",", Array)}]");
+SortedArray(Array);
+Console.WriteLine($"[{String.Join(",", Array)}]");
+
+
+int[] GetArray(int size)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(0,100);
+    }
+    return res;
+}
+void SortedArray(int[] number)
+{
+   
+int temp;
+
+for (int k=0;k<number.Length-1;k++)
+{
+    for (int j=k+1;j<number.Length;j++)
+    {
+        if (number[k]>number[j])
+        {
+            temp=number[k];
+            number[k]=number[j];
+            number[j]=temp;
+        }
+    }
+}
+
+ }
+
